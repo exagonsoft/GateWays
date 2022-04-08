7be2ab6b81ebaab7305b8ceda288245f684358c5
Alvaro R Martin <exagonsoft@gmail.com>
2022-04-08 17:40:10 +0200

    upd: Models
    Creating the Models to interact width the MySql Database

--- backend/src/main/java/com/gateways/backend/Models/GateWays.java
+++ backend/src/main/java/com/gateways/backend/Models/GateWays.java
@@ -1,5 +1,77 @@
 package com.gateways.backend.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class GateWays {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int PKID_GateWay;
+    private String SerialNumber;
+    private String GateWayName;
+    private String IPV4_Address;
+
+    public GateWays(){
+
+    }
+
+    /**
+     * @return int return the PKID_GateWay
+     */
+    public int getPKID_GateWay() {
+        return PKID_GateWay;
+    }
+
+    /**
+     * @param PKID_GateWay the PKID_GateWay to set
+     */
+    public void setPKID_GateWay(int PKID_GateWay) {
+        this.PKID_GateWay = PKID_GateWay;
+    }
+
+    /**
+     * @return String return the SerialNumber
+     */
+    public String getSerialNumber() {
+        return SerialNumber;
+    }
+
+    /**
+     * @param SerialNumber the SerialNumber to set
+     */
+    public void setSerialNumber(String SerialNumber) {
+        this.SerialNumber = SerialNumber;
+    }
+
+    /**
+     * @return String return the GateWayName
+     */
+    public String getGateWayName() {
+        return GateWayName;
+    }
+
+    /**
+     * @param GateWayName the GateWayName to set
+     */
+    public void setGateWayName(String GateWayName) {
+        this.GateWayName = GateWayName;
+    }
+
+    /**
+     * @return String return the IPV4_Address
+     */
+    public String getIPV4_Address() {
+        return IPV4_Address;
+    }
+
+    /**
+     * @param IPV4_Address the IPV4_Address to set
+     */
+    public void setIPV4_Address(String IPV4_Address) {
+        this.IPV4_Address = IPV4_Address;
+    }
+
 }
--- backend/src/main/java/com/gateways/backend/Models/Periferals.java
+++ backend/src/main/java/com/gateways/backend/Models/Periferals.java
@@ -1,5 +1,107 @@
 package com.gateways.backend.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Periferals {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int PKID_Periferal ;
+    private int UID_Number ;
+    private String Vendor;
+    private String  CreateDate;
+    private Boolean Status;
+    private int FKID_GateWay;
+
+    public Periferals() {
+
+    }
+
+    /**
+     * @return int return the PKID_Periferal
+     */
+    public int getPKID_Periferal() {
+        return PKID_Periferal;
+    }
+
+    /**
+     * @param PKID_Periferal the PKID_Periferal to set
+     */
+    public void setPKID_Periferal(int PKID_Periferal) {
+        this.PKID_Periferal = PKID_Periferal;
+    }
+
+    /**
+     * @return int return the UID_Number
+     */
+    public int getUID_Number() {
+        return UID_Number;
+    }
+
+    /**
+     * @param UID_Number the UID_Number to set
+     */
+    public void setUID_Number(int UID_Number) {
+        this.UID_Number = UID_Number;
+    }
+
+    /**
+     * @return String return the Vendor
+     */
+    public String getVendor() {
+        return Vendor;
+    }
+
+    /**
+     * @param Vendor the Vendor to set
+     */
+    public void setVendor(String Vendor) {
+        this.Vendor = Vendor;
+    }
+
+    /**
+     * @return String return the CreateDate
+     */
+    public String getCreateDate() {
+        return CreateDate;
+    }
+
+    /**
+     * @param CreateDate the CreateDate to set
+     */
+    public void setCreateDate(String CreateDate) {
+        this.CreateDate = CreateDate;
+    }
+
+    /**
+     * @return Boolean return the Status
+     */
+    public Boolean isStatus() {
+        return Status;
+    }
+
+    /**
+     * @param Status the Status to set
+     */
+    public void setStatus(Boolean Status) {
+        this.Status = Status;
+    }
+
+    /**
+     * @return int return the FKID_GateWay
+     */
+    public int getFKID_GateWay() {
+        return FKID_GateWay;
+    }
+
+    /**
+     * @param FKID_GateWay the FKID_GateWay to set
+     */
+    public void setFKID_GateWay(int FKID_GateWay) {
+        this.FKID_GateWay = FKID_GateWay;
+    }
+
 }
