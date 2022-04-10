0f1e49d6e929fb25e56c8fa75e3028db85fb75a8
Alvaro R Martin <exagonsoft@gmail.com>
2022-04-10 18:17:21 +0200

    upd: Adding Cross Origin
    Adding the CrossOrigin to the Spring Boot controllers to allow cross requests from the Frontend

--- backend/src/main/java/com/gateways/backend/Controllers/GateWaysController.java
+++ backend/src/main/java/com/gateways/backend/Controllers/GateWaysController.java
@@ -1,6 +1,7 @@
 package com.gateways.backend.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/gateways")
+@CrossOrigin
 public class GateWaysController {
     @Autowired
     private GateWaysService gatewaysService;
--- backend/src/main/java/com/gateways/backend/Controllers/PeripheralController.java
+++ backend/src/main/java/com/gateways/backend/Controllers/PeripheralController.java
@@ -2,6 +2,7 @@ package com.gateways.backend.Controllers;
 
 import com.gateways.backend.Services.PeripheralsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/peripherals")
+@CrossOrigin
 public class PeripheralController {
     @Autowired
     private PeripheralsService peripheralsService;
